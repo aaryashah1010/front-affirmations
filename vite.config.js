898cf39fc43faf4cca60d04d666c1381f070fc2f
Aarya <aaryashah1010@gmail.com>
2025-10-24 12:16:39 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+  import { defineConfig } from 'vite'
+  import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.[jt]sx?$/,
-    exclude: []
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx'
+  // https://vite.dev/config/
+  export default defineConfig({
+    base:'/',
+    plugins: [react()],
+    esbuild: {
+      loader: 'jsx',
+      include: /src\/.*\.[jt]sx?$/,
+      exclude: []
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        loader: {
+          '.js': 'jsx'
+        }
       }
     }
-  }
-})
+  })
